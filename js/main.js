6bed7080243f2f34e2394d3caaf903e397aef8e1
andrey <andrey@sublimestudio.es>
2025-05-05 21:25:12 +0200

    --wip-- [skip ci]

--- js/main.js
+++ js/main.js
@@ -32,14 +32,43 @@ function loadContacts() {
 function printData(data) {
     console.log("estas en print data: ",data)
     const dataList = document.getElementById("list")
-
+    dataList.innerHTML = ""
     data.forEach(contact => {
-        dataList.innerHTML += `<div class="bg-white shadow rounded-xl px-4 py-2">
-            <h1 class="font-bold text-lg text-blue-800">${contact.name}</h1>
-            <p class="px-2 py-0.5 bg-indigo-700 w-fit rounded-full text-white">${contact.email}</p>
-            <p class="text-slate-700">${contact.phone}</p>
+        dataList.innerHTML += ` <div class="bg-white shadow rounded-xl px-4 py-2 flex justify-between">
+            <div>
+                <h1 class="font-bold text-lg text-blue-800">${contact.name}</h1>
+                <p class="px-2 py-0.5 bg-indigo-700 w-fit rounded-full text-white">${contact.email}</p>
+                <p class="text-slate-700">${contact.phone}</p>
+            </div>
+            <div>
+                <button onclick="deleteContact('${contact.id}')" class="bg-red-200 hover:bg-red-500 rounded-lg px-4 py-2">Eliminar</button>
+            </div>
         </div>`
     });
 }
 
 loadContacts()
+
+function deleteContact(id) {
+    const url = baseUrl + "/rest/v1/contacts?id=eq." + id
+    const accessToken = localStorage.getItem("token")
+
+    const requestOptions =  {
+        method: "DELETE",
+        headers: {
+            "apiKey": apiKey,
+            "Authorization": `Bearer ${accessToken}`
+        }
+    }
+
+    fetch(url, requestOptions)
+    .then((res) => {
+        if (res.status == 204) {
+            alert("El contacto fue eliminado")
+            loadContacts()
+        }
+    })
+    .catch((error) => {
+        console.log("Ha ocurrido un error: ", error)
+    })
+}
